bcd007c1e4c347b710850fa57b60b84ee66a5225
Mahdi <Mahdi.nekuie@gmail.com>
2023-03-07 23:44:17 +0330

    Update main.ino

--- src/main.ino
+++ src/main.ino
@@ -130,6 +130,8 @@ void setup(void)
     // }
 
     // scroll = line_height;
+
+    drawkeypad();
 }
 
 
@@ -137,12 +139,13 @@ void setup(void)
 void loop(void)
 {
     // character = Keypad();
-    terminal_print(Keypad(), RED);
+    // terminal_print(Keypad(), RED);
+    // Serial.println(Keypad());
     
     while(Serial.available()>0)
     {
-    //     character = Serial.read();
-        terminal_print(Serial.read(), GREEN);
+        character = Serial.read();
+        terminal_print(character, GREEN);
     }
 
     // Touch_getXY();
@@ -202,9 +205,140 @@ byte i0 = 0;
 
 byte return_char = 0;
 
+int key_row = -1;
+int key_colum = -1;
+
 char Keypad()
 {
-    // tft.fillRect(0, tft.height() - footer_height, tft.width(), footer_height, TFT_LIGHTGREY);
+    if(Touch_getXY())
+    {
+        if (pixel_y - (header_height + terminal_height) > 0)
+        {
+            key_colum = pixel_x / 32;
+            key_row = (pixel_y - (header_height + terminal_height)) / 32;
+
+            key_x = (key_colum * 32) + 1;
+            key_y = (header_height + terminal_height) + (key_row * 32) + 1;
+            color = TFT_DARKGREY;
+
+            i0 = key_row + (sign * 5);
+            tft.fillRoundRect(key_x, key_y, key_w, key_h, 10, color);
+            tft.setTextSize(3);
+            tft.setTextColor(WHITE);
+            tft.setCursor(key_x + 7, key_y + 5);
+
+            if(isAlpha(char(keys[i0][key_colum])) && capslock) tft.print(char(keys[i0][key_colum] - 32));
+            else tft.print(char(keys[i0][key_colum]));
+
+            if(isAlpha(char(keys[i0][key_colum])) && capslock) return_char = char(keys[i0][key_colum] - 32);
+            else return_char = char(keys[i0][key_colum]);
+
+            switch (keys[key_row][key_colum])
+            {
+                case 7:
+                    keys[key_row][key_colum] = 8;
+                    capslock = !capslock;
+                    break;
+
+                case 8:
+                    keys[key_row][key_colum] = 7;
+                    capslock = !capslock;
+                break;
+
+                case 16:
+                    return_char = 13;
+                    Serial.println("> ");
+                break;
+
+                case 17:
+                    Serial.println("< ");
+                break;
+
+                case 23:
+                    sign = !sign;
+                break;
+
+                default:
+                break;
+            }
+        }
+    }
+    else
+    {
+        key_colum = -1;
+        key_row = -1;
+        return_char = 0;
+    }
+
+    terminal_print(return_char, RED);
+    // return return_char;
+
+    // Serial.print(key_colum);
+    // Serial.print("\t");
+    // Serial.println(key_row);
+
+    // // tft.fillRect(0, tft.height() - footer_height, tft.width(), footer_height, TFT_LIGHTGREY);
+    // return_char = 0;
+    // for (byte i = 0; i < 5; i++)
+    // {
+    //     for (byte j = 0; j < 10; j++)
+    //     {
+    //         key_x = (j * 32) + 1;
+    //         key_y = (header_height + terminal_height) + (i * 32) + 1;
+    //         color = TFT_DARKGREY;
+
+    //         if (Touch_getXY())
+    //         {
+    //             if ((key_x < pixel_x) && (key_x > (int16_t)(pixel_x - key_w)) && (key_y < pixel_y) && (key_y > (int16_t)(pixel_y - key_h)))
+    //             {
+    //                 if(isAlpha(char(keys[i0][j])) && capslock) return_char = char(keys[i0][j] - 32);
+    //                 else return_char = char(keys[i0][j]);
+    //                 // hold = true;
+    //                 color = TFT_BLACK;
+    //                 switch (keys[i][j])
+    //                 {
+    //                     case 7:
+    //                         keys[i][j] = 8;
+    //                         capslock = !capslock;
+    //                         break;
+
+    //                     case 8:
+    //                         keys[i][j] = 7;
+    //                         capslock = !capslock;
+    //                     break;
+
+    //                     case 16:
+    //                         return_char = 13;
+    //                         Serial.println("> ");
+    //                     break;
+
+    //                     case 17:
+    //                         Serial.println("< ");
+    //                     break;
+
+    //                     case 23:
+    //                         sign = !sign;
+    //                     break;
+
+    //                     default:
+    //                     break;
+    //                 }
+    //             }
+    //         }
+    //         i0 = i + (sign * 5);
+    //         tft.fillRoundRect(key_x, key_y, key_w, key_h, 10, color);
+    //         tft.setTextSize(3);
+    //         tft.setTextColor(WHITE);
+    //         tft.setCursor(key_x + 7, key_y + 5);
+
+    //         if(isAlpha(char(keys[i0][j])) && capslock) tft.print(char(keys[i0][j] - 32));
+    //         else tft.print(char(keys[i0][j]));
+    //     }
+    // }
+}
+
+void drawkeypad()
+{
     return_char = 0;
     for (byte i = 0; i < 5; i++)
     {
@@ -214,44 +348,7 @@ char Keypad()
             key_y = (header_height + terminal_height) + (i * 32) + 1;
             color = TFT_DARKGREY;
 
-            if (Touch_getXY())
-            {
-                if ((key_x < pixel_x) && (key_x > (int16_t)(pixel_x - key_w)) && (key_y < pixel_y) && (key_y > (int16_t)(pixel_y - key_h)))
-                {
-                    if(isAlpha(char(keys[i0][j])) && capslock) return_char = char(keys[i0][j] - 32);
-                    else return_char = char(keys[i0][j]);
-                    // hold = true;
-                    color = TFT_BLACK;
-                    switch (keys[i][j])
-                    {
-                        case 7:
-                            keys[i][j] = 8;
-                            capslock = !capslock;
-                            break;
 
-                        case 8:
-                            keys[i][j] = 7;
-                            capslock = !capslock;
-                        break;
-
-                        case 16:
-                            return_char = 13;
-                            Serial.println("> ");
-                        break;
-
-                        case 17:
-                            Serial.println("< ");
-                        break;
-
-                        case 23:
-                            sign = !sign;
-                        break;
-
-                        default:
-                        break;
-                    }
-                }
-            }
             i0 = i + (sign * 5);
             tft.fillRoundRect(key_x, key_y, key_w, key_h, 10, color);
             tft.setTextSize(3);
@@ -262,7 +359,6 @@ char Keypad()
             else tft.print(char(keys[i0][j]));
         }
     }
-    return return_char;
 }
 
 void terminal_print(char character_in, int color)
@@ -307,7 +403,7 @@ void terminal_print(char character_in, int color)
             {
                 line = 0;
                 scroll = 0;
-                tft.vertScroll(header_height, terminal_height, scroll);
+                // tft.vertScroll(header_height, terminal_height, scroll);
             }
 
             // tft.fillRect(0, scroll + header_height, tft.width(), line_height, TFT_BLACK);
